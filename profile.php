3684513ffd678560ab5faa5c9d035f21d46cf05e
mingng18 <92716196+mingng18@users.noreply.github.com>
2023-01-29 11:09:43 +0800

    Updated php

--- profile.php
+++ profile.php
@@ -19,3 +19,24 @@ if ($db->dbConnect()) {
 } else echo "Error: Database connection";
 
 ?>
+<?php
+require "DataBase.php";
+$db = new DataBase();
+if ($db->dbConnect()) {
+
+    //Paste ur sql code here
+    $userId = $db->prepareData($_POST['userId']);
+    $sql = "SELECT profile.userId, profile.baristaId, profile.adminId, profile.userPicUrl, profile.username, profile.email, profile.userStreetNo, profile.userTaman, profile.userPostalCode, profile.userState FROM profile WHERE userId='" . $userId . "'";
+        
+    $result = mysqli_query($db->getConnect(), $sql);
+    if (mysqli_num_rows($result) != 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+
+            //It will return the result here, split the string in Java
+            echo $row['userId'] . " - " . $row['userPicUrl'] . " - " . $row['baristaId'] . " - " . $row['adminId'] . " - " . $row['username'] . " - " . $row['email'] . " - " . $row['userStreetNo'] . " - " . $row['userTaman'] . " - " . $row['userPostalCode'] . " - " . $row['userState'];
+            
+        }
+    } else echo "No results";
+} else echo "Error: Database connection";
+
+?>
