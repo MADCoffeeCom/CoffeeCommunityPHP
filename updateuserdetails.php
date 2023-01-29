5b6e082cc80132e59d0cdd3ae201b0df20e131c6
mingng18 <92716196+mingng18@users.noreply.github.com>
2023-01-29 20:18:19 +0800

    update password fix

--- updateuserdetails.php
+++ updateuserdetails.php
@@ -1,16 +1,29 @@
 <?php
 require "DataBase.php";
 $db = new DataBase();
-if ($db->dbConnect()) {
+$db2 = new DataBase();
+if ($db->dbConnect() && $db2->dbConnect()) {
 
     //Paste ur sql code here
     $email = $db->prepareData($_POST['email']);
+    $userId = $db->prepareData($_POST['userId']);
+    $oldpassword = $db->prepareData($_POST['oldpassword']);
     $password = $db->prepareData($_POST['password']);
     $password = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "UPDATE profile SET email = '" . $email . "', password = '" . $password . "' WHERE userId = 'UID_abang'";
-    if (mysqli_query($db->getConnect(), $sql)){
-        echo "Update Success";
-    } else echo "No results";
+
+    $sql = "select * from `profile` where userId = '" . $userId . "'";
+    $result = mysqli_query($db->connect, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result) != 0) {
+        $dbuserID = $row['userId'];
+        $dbpassword = $row['password'];
+        if (password_verify($oldpassword, $dbpassword)) {
+            $sql2 = "UPDATE profile SET email = '" . $email . "', password = '" . $password . "' WHERE userId = '" . $userId . "'";
+            if (mysqli_query($db2->getConnect(), $sql2)){
+                echo "Update Success";
+            } else echo "No results";
+        } else echo "Old password wrong ";
+    } else echo "Old password wrong ";
 } else echo "Error: Database connection";
 
 ?>
